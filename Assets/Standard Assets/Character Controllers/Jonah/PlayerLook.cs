a51c48b61c546ec98a9512aa81b1602944498211
Mike Groot <magroot@hotmail.nl>
2018-11-21 11:02:28 +0100

    Project files

---
+++ Assets/Standard Assets/Character Controllers/Jonah/PlayerLook.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+	[SerializeField] private string MouseX, MouseY;
+	[SerializeField] private float mouseSensitivity;
+
+	[SerializeField] private Transform playerBody;
+
+	private float xAxisClamp;
+
+		private void Awake()
+		{
+			lockCursor();
+			xAxisClamp = 0.0f;
+		}
+
+		private void lockCursor()
+		{
+				Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		private void Update()
+		{
+			CameraRotation();
+		}
+
+		private void CameraRotation()
+		{
+			float mouseX = Input.GetAxis(MouseX) * mouseSensitivity * Time.deltaTime;
+			float mouseY = Input.GetAxis(MouseY) * mouseSensitivity * Time.deltaTime;
+
+			xAxisClamp += mouseY;
+
+			if (xAxisClamp > 90.0f)
+			{
+				xAxisClamp = 90.0f;
+				mouseY = 0.0f;
+				ClampXAxisRotToVal(270.0f);
+			}
+
+			else if (xAxisClamp < -90.0f)
+			{
+				xAxisClamp = -90.0f;
+				mouseY = 0.0f;
+			  ClampXAxisRotToVal(90.0f);
+			}
+			transform.Rotate(Vector3.left * mouseY);
+			playerBody.Rotate(Vector3.up * mouseX);
+		}
+			private void ClampXAxisRotToVal(float value)
+			{
+				Vector3 eulerRotation = transform.eulerAngles;
+				eulerRotation.x = value;
+				transform.eulerAngles = eulerRotation;
+			}
+}
---
+++ Assets/Standard Assets/Character Controllers/Jonah/PlayerMove.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField] private string HorizontalInputName;
+    [SerializeField] private string VertitalInputName;
+    private float MovementSpeed;
+
+    [SerializeField] private float WalkSpeed, RunSpeed;
+    // [SerializeField] private float RunBuildupSpeed;
+    [SerializeField] private KeyCode SprintKey;
+    [SerializeField] private KeyCode IsMoving;
+
+    public Text currentSpeed;
+
+    //Stamina bar
+    public Slider StaminaBar;
+    public float maxStamina;
+    public float staminaLoss;
+    public float staminaGain;
+    private bool isEmpty = false;
+
+
+    private CharacterController CharController;
+    private void Awake()
+    {
+        CharController = GetComponent<CharacterController>();
+        StaminaBar.maxValue = maxStamina;
+    }
+
+    void Update()
+    { 
+        //checkIfStaminaIsEmpty();
+        PlayerMovement();      
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(HorizontalInputName);
+        float vertInput = Input.GetAxis(VertitalInputName);
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        CharController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * MovementSpeed);
+        SetMovementSpeed();
+    }
+
+    private void SetMovementSpeed()
+    {
+        if (Input.GetKey(IsMoving) && Input.GetKey(SprintKey) /*&& isEmpty != true*/)
+        {
+            MovementSpeed = Mathf.Lerp(MovementSpeed, RunSpeed, 0.5f);
+            //maxStamina -= staminaLoss * Time.deltaTime;
+            //StaminaBar.value = maxStamina;
+         //   currentSpeed.text = "Running " + MovementSpeed;
+            
+        }
+        else 
+        {
+            //maxStamina += staminaGain * Time.deltaTime;
+            //StaminaBar.value = maxStamina;
+            MovementSpeed = Mathf.Lerp(MovementSpeed, WalkSpeed, 0.5f);
+        //    currentSpeed.text = "Walking " + MovementSpeed;
+
+        }
+    }
+
+    //public bool checkIfStaminaIsEmpty()
+    //{
+    //    if (StaminaBar.value <= 1.0f)
+    //    {
+    //        return isEmpty = true;
+    //    }
+    //    else
+    //    {
+    //        return isEmpty = false;
+    //    }
+    //}
+}
